bcafcdb40fcf72b423472fa8ea9378e2ae2a6ec1
Lakindu Akash <Lakinduakash@gmail.com>
2019-02-07 01:42:09 +0530

    Add some helps

--- sslforfreeautomate.sh
+++ sslforfreeautomate.sh
@@ -1,11 +1,13 @@
 
 #Please fill the paths according to apache configuration file
+#Change the permission to executable file
+#Execute script from downloaded directory of zip file and executeas root user
+
 
 $CERTIFICATE_PATH='';
 $PRIVATE_KEY_PATH='';
 $CA_BUNDLE_PATH= '';
 
-$DOWNLOAD_PATH='';
 
 $DOWNLOAD_FILENAME='sslforfree.zip';
 unzip $DOWNLOAD_FILENAME;
